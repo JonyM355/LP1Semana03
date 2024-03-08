3e7c204c239dc22102f19b8e1e19cb0c9bcbf08e
JonyM355 <joaoricardoam05@gmail.com>
2024-03-08 20:49:30 +0000

    Program that filter strings which has more than 3 and less than 6 characters

---
+++ RightSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                if (s.Length > 3 & s.Length <6) //If the string has more than 3
+                                                //and less than 6 characters
+                {
+                    Console.WriteLine(s);
+                }
+                if (s.Length > 6)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
