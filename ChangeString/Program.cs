b536f7a8c218a3ed9c399b762a49a2308021e174
JonyM355 <joaoricardoam05@gmail.com>
2025-03-06 23:59:48 +0000

    Changing a character on a string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Digite alguma coisa:");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("Digite o caráter que deseja remover:");
+            string c = Console.ReadLine();
+
+            char c1 = char.Parse(c);
+
+            string ns = "";
+
+            foreach (char l in s)
+            {
+                if (l == c1)
+                {
+                    ns += 'x';
+                }
+                else
+                {
+                    ns += l;
+                }
+            }
+
+            Console.WriteLine(ns);
+
+        }
+    }
+}
