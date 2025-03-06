91c58df105d83d75e77dc75137f774dda8ce232a
JonyM355 <joaoricardoam05@gmail.com>
2025-03-06 23:18:35 +0000

    Using foreach and .Length with strings

---
+++ RightSize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string p in args)
+            {
+                if (p.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+
+                if (p.Length > 3)
+                {
+                    Console.WriteLine($"{p} ");
+                }
+
+            }
+        }
+    }
+}
