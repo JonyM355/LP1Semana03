0ad0686718573b8a78ae4d034138006420fc439c
JonyM355 <joaoricardoam05@gmail.com>
2025-03-07 12:10:53 +0000

    Using enumeration flag for a player 'abilities'

---
+++ HeroPerk/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string s = args[0].ToLower();
+
+            Perks myperks = 0;
+
+            foreach (char c in s)
+            {
+                switch (c)
+                {
+                    case 'w': myperks ^= Perks.WarpShift; break;
+                    case 'a': myperks ^= Perks.AutoHeal; break;
+                    case 's': myperks ^= Perks.Stealth; break;
+                    case 'd': myperks ^= Perks.DoubleJump; break;
+                    default:
+                        Console.WriteLine("!Unknown perk!");
+                        return;
+                }
+            }
+
+            if (myperks == 0)
+            {
+                Console.WriteLine("!No perks at all!");
+            }
+            else
+            {
+                Console.WriteLine(myperks);
+            }
+
+            if ((myperks & (Perks.Stealth ^ Perks.DoubleJump))
+                == (Perks.Stealth ^ Perks.DoubleJump))
+            {
+                Console.WriteLine("!Silent jumper!");
+            }
+
+            if ((myperks & Perks.AutoHeal) == 0)
+            {
+                Console.WriteLine("!Not gonna make it!");
+            }
+        }
+    }
+}
